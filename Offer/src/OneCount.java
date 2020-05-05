04005643da0264b7aae46643c37b0bbfb202552a
xiaomessi <921721870@qq.com>
2020-05-05 22:35:39 +0800

    'update'

---
+++ Offer/src/OneCount.java
@@ -0,0 +1,21 @@
+/**
+ * Created by xiaoaxiao on 2020/5/5
+ * Description: 二进制中1的个数
+ */
+public class OneCount {
+    public static int NumberOf1(int n) {
+        String str = Integer.toBinaryString(n);
+        int count = 0;
+        for (char c : str.toCharArray()){
+            if (c == '1') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(NumberOf1(15));
+        System.out.println(NumberOf1(-15));
+    }
+}
--- newcoder/src/BuyOrNot.java
+++ newcoder/src/BuyOrNot.java
@@ -3,4 +3,24 @@
  * Description:
  */
 public class BuyOrNot {
+
+    public String val;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null){
+            return false;
+        }
+        if (this == obj){
+            return true;
+        }
+
+        if (!(obj instanceof BuyOrNot)){
+            return false;
+        }
+
+        BuyOrNot other = (BuyOrNot)obj;
+        return other.val.equals(this.val);
+
+    }
 }
